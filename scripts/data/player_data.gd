d24312d341f26e9a85e9b0e3e39dd0af698659f2
robos <robostep9829@gmail.com>
2026-07-07 14:13:06 +0300

    Created a model index

---
+++ scripts/data/player_data.gd
@@ -0,0 +1,6 @@
+extends Resource
+class_name PlayerData
+
+@export var character: PackedScene
+@export var name: String
+@export var series: String
---
+++ scripts/data/player_data_table.gd
@@ -0,0 +1,4 @@
+extends Resource
+class_name PlayerDataTable
+
+@export var rows: Array[PlayerData]
